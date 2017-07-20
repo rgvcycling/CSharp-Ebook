9ad8b9554973ccbaf3c5103a141a436e42f11021
rgvcycling <garza.r.tx@gmail.com>
2017-07-20 10:19:41 -0500

    Initial set up

---
+++ CSharp/CSharp/Finally.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class Finally
+    {
+        static void Main()
+        {
+            try
+            {
+                // get random number
+                // if the number is 0, an error will occur
+                // if the number is 1, the method returns
+                // otherwise, fall through to the end
+
+                // return random number 0 <= random # <=3
+                int random = new Random().Next(0, 3); 
+                if(random ==0)
+                {
+                    throw new Exception("Random # = 0. Exception thrown.");
+                }
+                if(random==1)
+                {
+                    Console.WriteLine("Random # = 1. Return is executed.");
+                    return;
+                }
+                Console.WriteLine("Random # is 2. Exited normally.");
+            }
+            finally
+            {
+                // This statement is executed before the Main() method exits
+                // it is reached when an exception is thrown
+                // Also reached after the return statement and is also reached in other cases too
+
+                Console.WriteLine("\nControl flow reaches the finally block");
+
+            }
+        }
+    }
+}
---
+++ CSharp/CSharp/UserDefinedExceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    public class InvalidAgedException:Exception
+    {
+        public InvalidAgedException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class TestUserDefinedException
+    {
+        static void validate(int age)
+        {
+            if(age<18)
+            {
+                throw new InvalidAgedException("Sorry, your age must be 18 or greater");
+            }
+        }
+    
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter your age:");
+                validate(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            catch (InvalidAgedException e)
+            {
+                // displays the error message that was thrown in the validate() 
+                // and continue with the program flow
+                Console.WriteLine(e);
+                Console.WriteLine("Execution of the rest of the code");
+            }
+        }
+    }
+}
