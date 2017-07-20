d38b436017f053933daeb6fa7f369acce0a958a3
rgvcycling <garza.r.tx@gmail.com>
2017-07-20 08:45:02 -0500

    Switching over from MS Git to Git

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CatchingErrors
+{
+    class Shapestry
+    {
+        static void Main()
+        {
+            try
+            {
+                throw new ArgumentException("Invalid x argument");
+            }
+            catch (ArgumentException ex) when (ex.Message == "Invalid x argument")
+            {
+                Console.WriteLine("X");
+            }
+            catch (ArgumentException ex) when (ex.Message == "Invalid y argument")
+            {
+                Console.WriteLine("Y");
+            }
+
+        }
+    }
+
+}
